fd0033723dbe93c0acf90f801f0e7a88affaabb5
Paul Virally <pvirally@gmail.com>
2024-02-07 10:57:51 -0500

    Removed unused `normalize` parameter
    Also added some docstrings

--- src/VkFFTCUDA.jl
+++ src/VkFFTCUDA.jl
@@ -7,8 +7,19 @@ using AbstractFFTs
 import CUDA: CuPtr, CuArray
 
 const libvkfft = "/usr/local/lib/libVkFFTCUDA.so"
-const VKFFT_MAX_FFT_DIMENSIONS = @ccall libvkfft.max_fft_dimensions()::Culonglong
+const VKFFT_MAX_FFT_DIMENSIONS = @ccall libvkfft.max_fft_dimensions()::Culonglong # The maximum number of dimensions that VkFFT supports (set at compile time, currently 4)
 
+"""
+    _vkffterr2string(err::UInt64)
+
+Converts a VkFFT error code to a string.
+
+# Arguments
+- `err::UInt64`: The error code
+
+# Returns
+- A string representation of the error code
+"""
 function _vkffterr2string(err::UInt64)
     if err == 0
         return "VKFFT_SUCCESS"
@@ -205,6 +216,20 @@ function _vkffterr2string(err::UInt64)
     end
 end
 
+"""
+    VkFFTPlan{T}(app::Ptr{Cvoid}, config::Ptr{Cvoid}, direction::Int32, dims::NTuple{N, Int} where N, region, buffer_size::AbstractArray{Int}, type::DataType, is_inplace::Bool)
+
+A struct that holds the plan for a VkFFT operation.
+
+# Fields
+- `app::Ptr{Cvoid}`: The equivalent of an FFTW plan but for VkFFT
+- `config::Ptr{Cvoid}`: A struct that holds the VkFFFT configuration
+- `direction::Int32`: -1 for forward, 1 for inverse
+- `dims::NTuple{N, Int} where N`: The size of the input array
+- `region`: The dimensions on which to perform the FFT
+- `buffer_size::AbstractArray{Int}`: The size of the buffer VkFFT will use (in bytes)
+- `is_inplace::Bool`: Whether the plan is in-place (for optimization)
+"""
 mutable struct VkFFTPlan{T <: Union{ComplexF32, ComplexF64}} <: AbstractFFTs.Plan{T}
     app::Ptr{Cvoid} # The equivalent of an FFTW plan but for VkFFT
     config::Ptr{Cvoid} # A struct that holds all of the config for VkFFT
@@ -213,11 +238,10 @@ mutable struct VkFFTPlan{T <: Union{ComplexF32, ComplexF64}} <: AbstractFFTs.Pla
     region
     buffer_size::AbstractArray{Int}
     is_inplace::Bool
-    is_normalized::Bool
     pinv::AbstractFFTs.Plan # Used to cache the inverse plan
 
-    function VkFFTPlan(app::Ptr{Cvoid}, config::Ptr{Cvoid}, direction::Int32, dims::NTuple{N, Int} where N, region, buffer_size::AbstractArray{Int}, type::DataType, is_inplace::Bool, is_normalized::Bool)
-        plan = new{type}(app, config, direction, dims, region, buffer_size, is_inplace, is_normalized)
+    function VkFFTPlan(app::Ptr{Cvoid}, config::Ptr{Cvoid}, direction::Int32, dims::NTuple{N, Int} where N, region, buffer_size::AbstractArray{Int}, type::DataType, is_inplace::Bool)
+        plan = new{type}(app, config, direction, dims, region, buffer_size, is_inplace)
         finalizer(plan) do plan
             try
                 pinv = plan.pinv
@@ -235,19 +259,51 @@ mutable struct VkFFTPlan{T <: Union{ComplexF32, ComplexF64}} <: AbstractFFTs.Pla
     end
 end
 
-function _new_config(fft_dim::Int64, buffer_dim::Int64, dims::AbstractArray{Int64}, omit_dims::AbstractArray{Int64}, num_batches::Int64, coalesced_memory::Int64, aim_threads::Int64, num_shared_banks::Int64, forward::Bool, double_precision::Bool, inplace::Bool, normalize::Bool)
-    return @ccall libvkfft.new_config(fft_dim::Culonglong, buffer_dim::Int64, dims::Ptr{Culonglong}, omit_dims::Ptr{Culonglong}, num_batches::Culonglong, coalesced_memory::Culonglong, aim_threads::Culonglong, num_shared_banks::Culonglong, forward::Cuchar, double_precision::Cuchar, inplace::Cuchar, normalize::Cuchar)::Ptr{Cvoid}
+"""
+    _new_config(fft_dim::Int64, buffer_dim::Int64, dims::AbstractArray{Int64}, omit_dims::AbstractArray{Int64}, num_batches::Int64, coalesced_memory::Int64, aim_threads::Int64, num_shared_banks::Int64, forward::Bool, double_precision::Bool, inplace::Bool)
+
+Wrapper for the VkFFT C function new_config.
+"""
+function _new_config(fft_dim::Int64, buffer_dim::Int64, dims::AbstractArray{Int64}, omit_dims::AbstractArray{Int64}, num_batches::Int64, coalesced_memory::Int64, aim_threads::Int64, num_shared_banks::Int64, forward::Bool, double_precision::Bool, inplace::Bool)
+    return @ccall libvkfft.new_config(fft_dim::Culonglong, buffer_dim::Int64, dims::Ptr{Culonglong}, omit_dims::Ptr{Culonglong}, num_batches::Culonglong, coalesced_memory::Culonglong, aim_threads::Culonglong, num_shared_banks::Culonglong, forward::Cuchar, double_precision::Cuchar, inplace::Cuchar)::Ptr{Cvoid}
 end
 
+"""
+    _new_app(config::Ptr{Cvoid}, res_ptr::Ref{Culonglong})
+
+Wrapper for the VkFFT C function new_app.
+"""
 function _new_app(config::Ptr{Cvoid}, res_ptr::Ref{Culonglong})
     return @ccall libvkfft.new_app(config::Ptr{Cvoid}, res_ptr::Ref{Culonglong})::Ptr{Cvoid}
 end
 
+"""
+    _fft(app::Ptr{Cvoid}, x::CuPtr{Cvoid}, y::CuPtr{Cvoid}, direction::Int32)
+
+Wrapper for the VkFFT C function fft.
+"""
 function _fft(app::Ptr{Cvoid}, x::CuPtr{Cvoid}, y::CuPtr{Cvoid}, direction::Int32)
     return @ccall libvkfft.fft(app::Ptr{Cvoid}, x::CuPtr{Cvoid}, y::CuPtr{Cvoid}, direction::Cint)::Cuint
 end
 
-function _make_plan(x::CuArray{T}, region, forward::Bool, inplace::Bool, normalize::Bool; coalesced_memory::Int=0, aim_threads::Int=0, num_shared_banks::Int=0) where T <: Union{ComplexF32, ComplexF64}
+"""
+    _make_plan(x::CuArray{T}, region, forward::Bool, inplace::Bool; coalesced_memory::Int=0, aim_threads::Int=0, num_shared_banks::Int=0)
+
+Creates a VkFFT plan for the given input array.
+
+# Arguments
+- `x::CuArray{T}`: The input array
+- `region`: The dimensions on which to perform the FFT
+- `forward::Bool`: Whether to perform a forward FFT
+- `inplace::Bool`: Whether to perform the FFT in-place
+- `coalesced_memory::Int=0`: The number of coalesced memory banks to use (0 for automatic)
+- `aim_threads::Int=0`: The number of threads to aim for (0 for automatic)
+- `num_shared_banks::Int=0`: The number of shared memory banks to use (0 for automatic)
+
+# Returns
+- A VkFFTPlan
+"""
+function _make_plan(x::CuArray{T}, region, forward::Bool, inplace::Bool; coalesced_memory::Int=0, aim_threads::Int=0, num_shared_banks::Int=0) where T <: Union{ComplexF32, ComplexF64}
     num_buffer_dim = ndims(x)
 
     dim_sizes = zeros(Int, VKFFT_MAX_FFT_DIMENSIONS)
@@ -263,7 +319,7 @@ function _make_plan(x::CuArray{T}, region, forward::Bool, inplace::Bool, normali
 
     double_precision = eltype(x) <: ComplexF64
 
-    config = _new_config(num_fft_dim, num_buffer_dim, dim_sizes, omit_dims, num_batches, coalesced_memory, aim_threads, num_shared_banks, forward, double_precision, inplace, normalize)
+    config = _new_config(num_fft_dim, num_buffer_dim, dim_sizes, omit_dims, num_batches, coalesced_memory, aim_threads, num_shared_banks, forward, double_precision, inplace)
     if config == C_NULL
         throw(ArgumentError("Fatal VkFFT error: Failed to create config. Was CUDA initialized?"))
     end
@@ -281,15 +337,37 @@ function _make_plan(x::CuArray{T}, region, forward::Bool, inplace::Bool, normali
     end
 
     direction = forward ? Int32(-1) : Int32(1)
-    return VkFFTPlan(app, config, direction, size(x), region, dim_sizes, eltype(x), inplace, normalize)
+    return VkFFTPlan(app, config, direction, size(x), region, dim_sizes, eltype(x), inplace)
 end
 
+"""
+    _delete_plan(plan::VkFFTPlan)
+
+Deletes a VkFFT plan.
+
+Wrapper for the VkFFT C functions delete_app and delete_config.
+"""
 function _delete_plan(plan::VkFFTPlan)
     @ccall libvkfft.delete_app(plan.app::Ptr{Cvoid})::Cvoid
     @ccall libvkfft.delete_config(plan.config::Ptr{Cvoid})::Cvoid
 end
 
-function _get_tuned_params(x::CuArray{T}, region, forward::Bool, inplace::Bool, normalize::Bool; num_ffts::Int=1000) where T <: Union{ComplexF32, ComplexF64}
+"""
+    _get_tuned_params(x::CuArray{T}, region, forward::Bool, inplace::Bool; num_ffts::Int=1000)
+
+Tunes the parameters for a VkFFT plan.
+
+# Arguments
+- `x::CuArray{T}`: The input array
+- `region`: The dimensions on which to perform the FFT
+- `forward::Bool`: Whether to perform a forward FFT
+- `inplace::Bool`: Whether to perform the FFT in-place
+- `num_ffts::Int=1000`: The number of FFTs to run to determine the best parameters
+
+# Returns
+- A tuple of the best parameters
+"""
+function _get_tuned_params(x::CuArray{T}, region, forward::Bool, inplace::Bool; num_ffts::Int=1000) where T <: Union{ComplexF32, ComplexF64}
     # The set of parameters to try
     coalesced_memory_vals = [32, 64, 128]
     aim_threads_vals = [32, 64, 128, 256]
@@ -298,7 +376,7 @@ function _get_tuned_params(x::CuArray{T}, region, forward::Bool, inplace::Bool, 
     best_time = Inf
     best_params = (0, 0, 0)
     for (coalesced_memory, aim_threads) in params
-        plan = _make_plan(x, region, forward, inplace, normalize; coalesced_memory=coalesced_memory, aim_threads=aim_threads)
+        plan = _make_plan(x, region, forward, inplace; coalesced_memory=coalesced_memory, aim_threads=aim_threads)
 
         # Run once to warm up (BenchmarkTools.jl does this too)
         res = plan * x
@@ -318,21 +396,36 @@ function _get_tuned_params(x::CuArray{T}, region, forward::Bool, inplace::Bool, 
     return best_params
 end
 
-function _make_tuned_plan(x::CuArray{T}, region, forward::Bool, inplace::Bool, normalize::Bool; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64}
+"""
+    _make_tuned_plan(x::CuArray{T}, region, forward::Bool, inplace::Bool; tune::Bool=false)
+
+Creates a tuned VkFFT plan for the given input array.
+
+# Arguments
+- `x::CuArray{T}`: The input array
+- `region`: The dimensions on which to perform the FFT
+- `forward::Bool`: Whether to perform a forward FFT
+- `inplace::Bool`: Whether to perform the FFT in-place
+- `tune::Bool=false`: Whether to tune the parameters
+
+# Returns
+- A VkFFTPlan
+"""
+function _make_tuned_plan(x::CuArray{T}, region, forward::Bool, inplace::Bool; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64}
     if tune
-        coalesced_memory, aim_threads = _get_tuned_params(x, region, forward, inplace, normalize)
+        coalesced_memory, aim_threads = _get_tuned_params(x, region, forward, inplace)
     else
         coalesced_memory, aim_threads = (0, 0)
     end
-    return _make_plan(x, region, forward, inplace, normalize; coalesced_memory=coalesced_memory, aim_threads=aim_threads)
+    return _make_plan(x, region, forward, inplace; coalesced_memory=coalesced_memory, aim_threads=aim_threads)
 end
 
 Base.size(plan::VkFFTPlan) = tuple(plan.buffer_size...)
 Base.eltype(plan::VkFFTPlan) = typeof(plan).parameters[1]
-AbstractFFTs.plan_fft(x::CuArray{T}, region; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64} = _make_tuned_plan(x, region, true, false, false; tune=tune)
-AbstractFFTs.plan_fft!(x::CuArray{T}, region; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64} = _make_tuned_plan(x, region, true, true, false; tune=tune)
-AbstractFFTs.plan_bfft(x::CuArray{T}, region; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64} = _make_tuned_plan(x, region, false, false, false; tune=tune)
-AbstractFFTs.plan_bfft!(x::CuArray{T}, region; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64} = _make_tuned_plan(x, region, false, true, false; tune=tune)
+AbstractFFTs.plan_fft(x::CuArray{T}, region; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64} = _make_tuned_plan(x, region, true, false; tune=tune)
+AbstractFFTs.plan_fft!(x::CuArray{T}, region; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64} = _make_tuned_plan(x, region, true, true; tune=tune)
+AbstractFFTs.plan_bfft(x::CuArray{T}, region; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64} = _make_tuned_plan(x, region, false, false; tune=tune)
+AbstractFFTs.plan_bfft!(x::CuArray{T}, region; tune::Bool=false) where T <: Union{ComplexF32, ComplexF64} = _make_tuned_plan(x, region, false, true; tune=tune)
 
 function AbstractFFTs.plan_inv(plan::VkFFTPlan)
     # Construct a fake x from plan, and pass that to _make_plan
@@ -341,7 +434,7 @@ function AbstractFFTs.plan_inv(plan::VkFFTPlan)
     x = CuArray{eltype(plan)}(undef, tuple(dims...))
     forward = plan.direction == Int32(-1)
 
-    invplan = _make_plan(x, region, !forward, plan.is_inplace, false)
+    invplan = _make_plan(x, region, !forward, plan.is_inplace)
     return AbstractFFTs.ScaledPlan(invplan, AbstractFFTs.normalization(x, region))
 end
 
