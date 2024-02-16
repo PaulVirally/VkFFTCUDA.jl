38e22d4fe5d485936dee4ea36f1e1516ebd5c22d
Paul Virally <pvirally@gmail.com>
2024-02-16 11:26:11 -0500

    Added back the tests from AbstractFFTs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
-# using CUDA, AbstractFFTs, VkFFTCUDA
+using CUDA, AbstractFFTs, VkFFTCUDA, Test
 
-# AbstractFFTs.TestUtils.test_complex_ffts(CuArray{ComplexF32}, test_inplace=true, test_adjoint=false)
+AbstractFFTs.TestUtils.test_complex_ffts(CuArray{ComplexF32}, test_inplace=true, test_adjoint=false)
+AbstractFFTs.TestUtils.test_complex_ffts(CuArray{ComplexF64}, test_inplace=true, test_adjoint=false)
